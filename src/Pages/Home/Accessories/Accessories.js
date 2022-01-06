59f244fe4874f6862469815904cd166c2f0233fe
ihasanarnob <hasanarnob3@gmail.com>
2022-01-06 21:49:53 +0600

    accessories page  and responsive modified

--- src/Pages/Home/Accessories/Accessories.js
+++ src/Pages/Home/Accessories/Accessories.js
@@ -4,72 +4,65 @@ import './Accessories.css';
 import shop1 from '../../../Images/shop-1.jpg';
 import shop2 from '../../../Images/shop-2.jpg';
 import shop3 from '../../../Images/shop-3.jpg';
-import shop4 from '../../../Images/shop-4.jpg';
 import shop5 from '../../../Images/shop-5.jfif';
-import { Card, Col, Row } from 'react-bootstrap';
 
 const Accessories = () => {
     return (
         <div className="container my-3">
             <h2 className="mt-5 mb-3">Shop Accessories Item</h2>
-            <Row xs={1} md={3} className="g-3">
-                
-                <Col>
-                <Card>
-                    <Card.Img variant="top" src={shop2} />
-                    <Card.Body>
-                    <Card.Title>Goggles & Gloves</Card.Title>
-                    <Card.Text>
-                        Price:$76
-                    </Card.Text>
-                    </Card.Body>
-                </Card>
-                </Col>
-                <Col>
-                <Card>
-                    <Card.Img variant="top" src={shop3} />
-                    <Card.Body>
-                    <Card.Title>Canvas Shoes</Card.Title>
-                    <Card.Text>
-                    Price:$80
-                    </Card.Text>
-                    </Card.Body>
-                </Card>
-                </Col>
-                <Col>
-                <Card>
-                    <Card.Img variant="top" src={shop5} />
-                    <Card.Body>
-                    <Card.Title>Mavericks Helmet</Card.Title>
-                    <Card.Text>
-                    Price:$69
-                    </Card.Text>
-                    </Card.Body>
-                </Card>
-                </Col>
-                <Col>
-                <Card>
-                    <Card.Img variant="top" src={shop1} />
-                    <Card.Body>
-                    <Card.Title>Driving Jacket</Card.Title>
-                    <Card.Text>
-                        Price : $100 
-                    </Card.Text>
-                    </Card.Body>
-                </Card>
-                </Col>
-                <Col>
-                <Card>
-                    <Card.Img variant="top" src={shop4} />
-                    <Card.Body>
-                    <Card.Title>ATV Tires</Card.Title>
-                    <Card.Text>
-                    Price:$60
-                    </Card.Text>
-                    </Card.Body>
-                </Card>
-                </Col>
-            </Row>
+            {/* ................ */}
+            <div className="profile-area">
+                <div className="container">
+                    <div className="row row-cols-2 row-cols-md-4 g-4  ">
+                        <div className="col">
+                            <div className="card">
+                                <div className="img1"><img src={shop2} className="card-img-top" alt="..." /></div>
+
+                                <div className="main-text">
+                                    <h3>Goggles & Gloves</h3>
+                                    <h5> Price:$76</h5>
+                                </div>
+
+
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="card">
+                                <div className="img1"><img src={shop3} className="card-img-top" alt="..." /></div>
+
+                                <div className="main-text">
+                                    <h3>Canvas <br /> Shoes</h3>
+                                    <h5>Price:$80</h5>
+                                </div>
+
+
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="card">
+                                <div className="img1"><img src={shop5} className="card-img-top" alt="..." /></div>
+
+                                <div className="main-text">
+                                    <h3>Mavericks Helmet</h3>
+                                    <h5>Price:$90</h5>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="card">
+                                <div className="img1"><img src={shop1} className="card-img-top" alt="..." /></div>
+
+                                <div className="main-text">
+                                    <h3>Driving <br />  Jacket</h3>
+                                    <h5>Price:$90</h5>
+                                </div>
+                            </div>
+                        </div>
+                        {/* ................. */}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
